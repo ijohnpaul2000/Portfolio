12dbfdca5f04cde2cc11b6a5916d5247dc56e12d
John Paul Pineda <57859589+ijohnpaul2000@users.noreply.github.com>
2022-02-12 21:16:20 +0800

    deploy fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
 import ProjectsPage from "./pages/ProjectsPage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" exact element={<HomePage />} />
-        <Route path="/projects" element={<ProjectsPage />} />
-      </Routes>
-    </BrowserRouter>
+    <HashRouter>
+      <App />
+    </HashRouter>
   );
 }
 
